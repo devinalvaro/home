35f96f3446d3133c9d0c6f506ffd796e2c8f36ab
Devin Alvaro <devin.alvaro@gmail.com>
2021-04-28 22:25:03 +0800

    Use emacs with native-comp

--- editor/emacs.nix
+++ editor/emacs.nix
@@ -1,8 +1,15 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+    }))
+  ];
+
   programs.emacs = {
     enable = true;
+    package = pkgs.emacsGcc;
     extraPackages = epkgs: [ epkgs.vterm ];
   };
 
