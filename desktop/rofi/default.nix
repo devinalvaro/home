637e9967ec201ba24b0be574ba43ecf56bb9b0a9
Devin Alvaro <devin.alvaro@gmail.com>
2021-04-30 22:05:45 +0800

    Rebind Control-j/k in rofi

--- desktop/rofi/default.nix
+++ desktop/rofi/default.nix
@@ -8,8 +8,14 @@ in
     terminal = "alacritty";
     theme = ./nord.rasi;
     extraConfig = {
+      # General:
       modi = "window,run";
       disable-history = true;
+      # Bindings:
+      kb-remove-to-eol = ""; # Unbind Control+k.
+      kb-accept-entry = "Control+m,Return,KP_Enter"; # Unbind Control+j.
+      kb-row-up = "Up,Control+p,ISO_Left_Tab,Control+k"; # Bind Control+k.
+      kb-row-down = "Down,Control+n,Control+j"; # Bind Control+j.
     };
   };
 }
