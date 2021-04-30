b92297a3af1f90e4c3e7900f9e424dd571b4a018
Devin Alvaro <devin.alvaro@gmail.com>
2021-05-01 04:03:36 +0800

    Unuse emacsGcc

--- editor/emacs.nix
+++ editor/emacs.nix
@@ -1,15 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-    }))
-  ];
-
   programs.emacs = {
     enable = true;
-    package = pkgs.emacsGcc;
     extraPackages = epkgs: [ epkgs.vterm ];
   };
 
