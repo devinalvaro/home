ff9b3db3a10c08df03f0f9163d5d7d6a406a8047
Devin Alvaro <devin.alvaro@gmail.com>
2021-05-03 22:06:11 +0800

    Upgrade stateVersion

--- common.nix
+++ common.nix
@@ -9,7 +9,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.03";
+  home.stateVersion = "21.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
