54421e07b265fe50bcd072db54d7c2c5c10f1bec
Devin Alvaro <devin.alvaro@gmail.com>
2021-05-05 14:05:22 +0800

    Add .go/bin to PATH

--- modules/lang/go.nix
+++ modules/lang/go.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   programs.go = {
     enable = true;
     goPath = ".go";
   };
+
+  home.sessionPath = [ "${config.home.homeDirectory}/${config.programs.go.goPath}/bin" ];
 
   home.packages = with pkgs; [ gopls ];
 }
